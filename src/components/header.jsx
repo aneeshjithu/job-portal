61bed48b7478641cf83e505bc9a48eae8a2d8fe7
Aneesh <aneeshchandran06@gmail.com>
2024-12-26 01:16:46 +0530

    magic ui added,supabase added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import ShinyButton from "./ui/shiny-button";
 
 const Header = () => {
   return (
     <>
-      <nav className="flex  justify-between items-center my-2">
+      <nav className="flex  justify-between items-center my-2 mr-2">
         <Link>
           <img src="/pool.png" className="h-14 w-32" />
         </Link>
 
-        <Button variant="outline">Login</Button>
+        {/* <Button variant="outline">Login</Button> */}
+
+        <SignedOut>
+          <SignInButton>
+            <ShinyButton variant="outline" className="">
+              Login
+            </ShinyButton>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </nav>
     </>
   );
--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -5,16 +5,16 @@ import Header from "@/components/header";
 
 const AppLayout = () => {
   return (
-    <div>
+    <>
       <div className="grid-background"></div>
-      <main className="min-h-screen container">
+      <main className="min-h-screen">
         <Header />
         <Outlet />
       </main>
       <div className="p-10 text-center bg-gray-800 mt-10">
         this is my first fullstack Project
       </div>
-    </div>
+    </>
   );
 };
 
